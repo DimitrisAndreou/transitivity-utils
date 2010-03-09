b2aa4b25e9464b0f6c05183674563f38767b7a9f
jim.andreou <jim.andreou@0bbc48d0-2794-11df-ab0a-9f3f633ae91d>
2010-03-09 17:41:21 +0000

    Making serializable the implementations OrderList, TransitiveRelation, TransitiveBiRelation, Navigator.

--- trunk/test/edu/bath/transitivityutils/NavigatorsTest.java
+++ trunk/test/edu/bath/transitivityutils/NavigatorsTest.java
@@ -38,4 +38,24 @@ public class NavigatorsTest {
         assertEquals(ImmutableSet.of("b"), ImmutableSet.copyOf(navigator.related("b")));
         assertEquals(ImmutableSet.of(), ImmutableSet.copyOf(navigator.related("a")));
     }
+
+    @Test
+    public void testSerializable() {
+        Navigator<String> navigator = SerializationUtils.serializedCopy(Navigators.forMultimap(ImmutableSetMultimap.of("a", "a1", "a", "a2", "b", "b")));
+
+        assertEquals(ImmutableSet.of("a1", "a2"), ImmutableSet.copyOf(navigator.related("a")));
+        assertEquals(ImmutableSet.of("b"), ImmutableSet.copyOf(navigator.related("b")));
+        assertEquals(ImmutableSet.of(), ImmutableSet.copyOf(navigator.related("c")));
+
+        
+        navigator = SerializationUtils.serializedCopy(Navigators.invert(
+                Navigators.forMultimap(ImmutableSetMultimap.of("a", "a1", "a", "a2", "b", "b"))));
+
+        assertEquals(ImmutableSet.of("a1", "a2", "b"), ImmutableSet.copyOf(navigator.domain()));
+
+        assertEquals(ImmutableSet.of("a"), ImmutableSet.copyOf(navigator.related("a1")));
+        assertEquals(ImmutableSet.of("a"), ImmutableSet.copyOf(navigator.related("a2")));
+        assertEquals(ImmutableSet.of("b"), ImmutableSet.copyOf(navigator.related("b")));
+        assertEquals(ImmutableSet.of(), ImmutableSet.copyOf(navigator.related("a")));
+    }
 }
