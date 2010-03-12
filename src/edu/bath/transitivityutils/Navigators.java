88250f2b0eb498ea08b4fe1b781d3fd5880d17f2
jim.andreou <jim.andreou@0bbc48d0-2794-11df-ab0a-9f3f633ae91d>
2010-03-12 12:35:27 +0000

    Introducing Navigators#forFunction().

--- src/edu/bath/transitivityutils/Navigators.java
+++ src/edu/bath/transitivityutils/Navigators.java
@@ -1,5 +1,6 @@
 package edu.bath.transitivityutils;
 
+import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
@@ -17,6 +18,14 @@ public final class Navigators {
         return new MultimapNavigator<E>(Preconditions.checkNotNull(multimap));
     }
 
+    //view
+    //serializable if domain and navigationFunction is
+    public static <E> Navigator<E> forFunction(Set<E> domain,
+            Function<? super E, ? extends Set<E>> navigationFunction) {
+        return new FunctionNavigator<E>(Preconditions.checkNotNull(domain),
+                Preconditions.checkNotNull(navigationFunction));
+    }
+
     public static <E> Navigator<E> invert(Navigator<E> navigator) {
         SetMultimap<E, E> relationships = HashMultimap.create();
         for (E subject : navigator.domain()) {
@@ -27,6 +36,27 @@ public final class Navigators {
         return forMultimap(relationships);
     }
 
+    private static class FunctionNavigator<E> implements Navigator<E>, Serializable {
+        private final Set<E> domain;
+        private final Function<? super E, ? extends Set<E>> navigationFunction;
+
+        private static final long serialVersionUID = 6024090827962229701L;
+
+        FunctionNavigator(Set<E> domain,
+                Function<? super E, ? extends Set<E>> navigationFunction) {
+            this.domain = domain;
+            this.navigationFunction = navigationFunction;
+        }
+
+        public Set<E> related(E subjectValue) {
+            return navigationFunction.apply(subjectValue);
+        }
+
+        public Set<E> domain() {
+            return domain;
+        }
+    }
+    
     private static class MultimapNavigator<E> implements Navigator<E>, Serializable {
         private final SetMultimap<E, E> multimap;
 
--- test/edu/bath/transitivityutils/NavigatorsTest.java
+++ test/edu/bath/transitivityutils/NavigatorsTest.java
@@ -1,8 +1,10 @@
 package edu.bath.transitivityutils;
 
-import com.google.common.collect.ImmutableMultimap;
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSetMultimap;
+import java.io.Serializable;
+import java.util.Set;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -12,7 +14,7 @@ import static org.junit.Assert.*;
  */
 public class NavigatorsTest {
     @Test
-    public void testBasic() {
+    public void testForMultimap_related() {
         Navigator<String> navigator = Navigators.forMultimap(ImmutableSetMultimap.of("a", "a1", "a", "a2", "b", "b"));
         assertEquals(ImmutableSet.of("a1", "a2"), ImmutableSet.copyOf(navigator.related("a")));
         assertEquals(ImmutableSet.of("b"), ImmutableSet.copyOf(navigator.related("b")));
@@ -20,10 +22,24 @@ public class NavigatorsTest {
     }
 
     @Test
-    public void testDomain() {
+    public void testForMultimap_domain() {
         Navigator<String> navigator = Navigators.forMultimap(ImmutableSetMultimap.of("a", "a1", "a", "a2", "b", "b"));
 
         assertEquals(ImmutableSet.of("a", "b"), ImmutableSet.copyOf(navigator.domain()));
+    }
+
+    @Test
+    public void testForFunction() {
+        Navigator<Integer> navigator = Navigators.forFunction(ImmutableSet.of(1, 2, 3),
+            new Function<Integer, Set<Integer>>() {
+                public Set<Integer> apply(Integer value) {
+                    return ImmutableSet.of(value * 2, value + 1);
+                }
+        });
+        assertEquals(ImmutableSet.of(1, 2, 3), ImmutableSet.copyOf(navigator.domain()));
+        assertEquals(ImmutableSet.of(2, 2), ImmutableSet.copyOf(navigator.related(1)));
+        assertEquals(ImmutableSet.of(4, 3), ImmutableSet.copyOf(navigator.related(2)));
+        assertEquals(ImmutableSet.of(6, 4), ImmutableSet.copyOf(navigator.related(3)));
     }
 
     @Test
@@ -40,7 +56,7 @@ public class NavigatorsTest {
     }
 
     @Test
-    public void testSerializable() {
+    public void testForMultimap_Serializable() {
         Navigator<String> navigator = SerializationUtils.serializedCopy(Navigators.forMultimap(ImmutableSetMultimap.of("a", "a1", "a", "a2", "b", "b")));
 
         assertEquals(ImmutableSet.of("a1", "a2"), ImmutableSet.copyOf(navigator.related("a")));
@@ -58,4 +74,20 @@ public class NavigatorsTest {
         assertEquals(ImmutableSet.of("b"), ImmutableSet.copyOf(navigator.related("b")));
         assertEquals(ImmutableSet.of(), ImmutableSet.copyOf(navigator.related("a")));
     }
+
+    @Test
+    public void testForFunction_Serializable() {
+        Navigator<Integer> navigator = SerializationUtils.serializedCopy(
+                Navigators.forFunction(ImmutableSet.of(1, 2, 3), new MyFun()));
+
+        assertEquals(ImmutableSet.of(1, 2, 3), ImmutableSet.copyOf(navigator.domain()));
+        assertEquals(ImmutableSet.of(2, 2), ImmutableSet.copyOf(navigator.related(1)));
+        assertEquals(ImmutableSet.of(4, 3), ImmutableSet.copyOf(navigator.related(2)));
+        assertEquals(ImmutableSet.of(6, 4), ImmutableSet.copyOf(navigator.related(3)));
+   }
+    private static class MyFun implements Function<Integer, Set<Integer>>, Serializable {
+        public Set<Integer> apply(Integer value) {
+            return ImmutableSet.of(value * 2, value + 1);
+        }
+    };
 }
